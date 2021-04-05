8caae7c48c355b4a205e1b81ac077dbb5145f8a8
calvin <calvin.francis96@outlook.com>
2021-04-04 23:36:33 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+function wrapAdjective(visualFlair = "*") {
+    return function (adjective) {
+        return `You are ${visualFlair}${adjective}${visualFlair}!`
+    }
+}
+
+// function saturdayFun(activity = 'roller-skate') {
+//     return `I like to ${activity} on saturdays.`;
+// } (my version which could not get the test to pass)
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+// let mondayWork = function(toDo = 'go to the office') {
+//     return `I have to ${toDo} on Monday.`
+// } (my version which could not get the test to pass)
+let mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+let Calculator = {
+    add: function (num1, num2) {
+        return num1 + num2;
+    },
+    subtract: function (num1, num2) {
+        return num1 - num2;
+    },
+    multiply: function (num1, num2) {
+        return num1 * num2;
+    },
+    divide: function (num1, num2) {
+        return num1 / num2;
+    }
+};
+
+// function actionApplyer(startInt, funcArray) {
+//     let num = startInt;
+//     if (!funcArray) {
+//         return num;
+//     }
+//     for (i = 0; i < funcArray.length; i++) {
+//         num = funcArray[i](num);
+//     }
+//     return num;
+// } (my version which could not get the test to pass)
+let actionApplyer = function (start, ray) {
+    let a = start
+
+    for (let i = 0; i < ray.length; i++) {
+        a = ray[i](a)
+    }
+
+    return a
+}
